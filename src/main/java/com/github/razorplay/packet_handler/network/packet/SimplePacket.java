30c5a83a22d01f16d34a987e18ec485633ed1fa0
Nex <nexxuscontact@gmail.com>
2025-06-06 21:38:45 +0200

    feat 📦: created simple packet with override methods

---
+++ src/main/java/com/github/razorplay/packet_handler/network/packet/SimplePacket.java
@@ -0,0 +1,26 @@
+package com.github.razorplay.packet_handler.network.packet;
+
+import com.github.razorplay.packet_handler.exceptions.PacketSerializationException;
+import com.github.razorplay.packet_handler.network.IPacket;
+import com.github.razorplay.packet_handler.network.network_util.PacketDataSerializer;
+import com.github.razorplay.packet_handler.network.reflection.ClassSerializer;
+import com.github.razorplay.packet_handler.network.reflection.element.AnnotatedElementContext;
+import com.github.razorplay.packet_handler.util.StringUtil;
+
+public interface SimplePacket extends IPacket {
+
+    @Override
+    default String getPacketId() {
+        return StringUtil.toSnakeCase(this.getClass().getSimpleName());
+    }
+
+    @Override
+    default void write(final PacketDataSerializer serializer) throws PacketSerializationException {
+        ClassSerializer.encode(serializer, AnnotatedElementContext.ofClass(this));
+    }
+
+    @Override
+    default void read(final PacketDataSerializer serializer) throws PacketSerializationException {
+        throw new PacketSerializationException("Simple packets could not be deserialized with the read method. Instead use ClassSerializer#decode");
+    }
+}
